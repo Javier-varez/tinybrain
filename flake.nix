db1b009e2b898d58458b70962c230d117564f254
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2025-04-28 12:29:41 +0200

    Add flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    dbt.url = "github:javier-varez/dbt";
+  };
+
+  outputs =
+    { nixpkgs, dbt, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      shell = pkgs.mkShellNoCC {
+        packages = [
+          dbt.packages.${system}.dbt
+          pkgs.bluespec
+        ];
+      };
+    in
+    {
+      devShells.${system}.default = shell;
+    };
+}
