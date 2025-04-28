57eb07be52da8334a0bb0fe84ad594e1cc4fcd95
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2025-04-28 18:05:12 +0200

    Support resource files for simulation

--- RULES/tinybrain/bluespec/simulation.go
+++ RULES/tinybrain/bluespec/simulation.go
@@ -2,6 +2,7 @@ package bluespec
 
 import (
 	"fmt"
+	"path/filepath"
 	"strings"
 
 	"dbt-rules/RULES/core"
@@ -18,6 +19,9 @@ type Bluesim struct {
 	TopModule string
 	Deps      []Dep
 	Flags     []string
+
+	// Resources required during simulation
+	Resources []core.Path
 }
 
 func (b Bluesim) intermediatesDir() core.OutPath {
@@ -36,11 +40,41 @@ func (b Bluesim) infoDir() core.OutPath {
 	return b.intermediatesDir().WithSuffix("/info")
 }
 
+func (b Bluesim) resourcesDir() core.OutPath {
+	return b.Out.WithSuffix("_resources")
+}
+
+func (b Bluesim) copiedResources() []core.OutPath {
+	resources := []core.OutPath{}
+
+	for _, src := range b.Resources {
+		target := b.resourcesDir().WithSuffix(fmt.Sprintf("/%s", filepath.Base(src.String())))
+		resources = append(resources, target)
+	}
+	return resources
+}
+
 func (b Bluesim) Build(ctx core.Context) {
 	importPaths := []string{
 		"%/Libraries",
 	}
-	inputs := []core.Path{}
+	inputs := []core.Path{b.TestBench}
+
+	if len(b.Resources) != 0 {
+		copiedResources := b.copiedResources()
+		script := executeTemplate(copySrcsTemplate, &copySrcsArgs{
+			TargetDir: b.resourcesDir(),
+			Srcs:      b.Resources,
+		})
+
+		ctx.AddBuildStep(core.BuildStep{
+			Outs:   copiedResources,
+			Ins:    b.Resources,
+			Script: script,
+		})
+
+		inputs = append(inputs, asInputs(copiedResources)...)
+	}
 
 	allDeps := []Library{}
 	for _, dep := range b.Deps {
@@ -58,8 +92,8 @@ func (b Bluesim) Build(ctx core.Context) {
 
 	flags := strings.Join(b.Flags, " ")
 
-	cmd := fmt.Sprintf("rm -rf %q %q %q %q", b.simDir().Absolute(), b.bluespecDir().Absolute(), b.infoDir(), b.Out.Absolute())
-	cmd = fmt.Sprintf("%s && mkdir -p %q %q %q", cmd, b.simDir().Absolute(), b.bluespecDir().Absolute(), b.infoDir())
+	cmd := fmt.Sprintf("rm -rf %q %q %q %q", b.simDir().Absolute(), b.bluespecDir().Absolute(), b.infoDir().Absolute(), b.Out.Absolute())
+	cmd = fmt.Sprintf("%s && mkdir -p %q %q %q", cmd, b.simDir().Absolute(), b.bluespecDir().Absolute(), b.infoDir().Absolute())
 	cmd = fmt.Sprintf("%s && bsc -quiet --sim %s --simdir %q --bdir %q --info-dir %q -p %q -g %q %q", cmd, flags, b.simDir(), b.bluespecDir(), b.infoDir(), strings.Join(importPaths, ":"), b.TopModule, b.TestBench.Absolute())
 	cmd = fmt.Sprintf("%s && bsc -quiet -e %q -sim -o %q %s --simdir %q --bdir %q --info-dir %q -p %q -g %q", cmd, b.TopModule, b.Out.Absolute(), flags, b.simDir(), b.bluespecDir(), b.infoDir(), strings.Join(importPaths, ":"), b.TopModule)
 
@@ -71,5 +105,5 @@ func (b Bluesim) Build(ctx core.Context) {
 }
 
 func (b Bluesim) Test(args []string) string {
-	return fmt.Sprintf("%q %s", b.Out.Absolute(), strings.Join(args, " "))
+	return fmt.Sprintf("cd %q && %q %s", b.resourcesDir(), b.Out.Absolute(), strings.Join(args, " "))
 }
