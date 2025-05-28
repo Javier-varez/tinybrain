097cdaa305c8afcff64c574098855a432e12b732
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2025-05-28 22:27:17 +0200

    Add support for aarch64

--- flake.nix
+++ flake.nix
@@ -1,25 +1,37 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     dbt.url = "github:javier-varez/dbt";
   };
 
   outputs =
     { nixpkgs, dbt, ... }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      shell = pkgs.mkShellNoCC {
-        packages = [
-          dbt.packages.${system}.dbt
-          pkgs.bluespec
-          pkgs.gtkwave
-          pkgs.probe-rs
-          pkgs.flip-link
-        ];
-      };
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+
+      shellForSystem =
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.mkShellNoCC {
+          packages = [
+            dbt.packages.${system}.dbt
+            pkgs.bluespec
+            pkgs.gtkwave
+            pkgs.probe-rs
+            pkgs.flip-link
+          ];
+
+        };
+
     in
     {
-      devShells.${system}.default = shell;
+      devShell = forAllSystems shellForSystem;
     };
 }
