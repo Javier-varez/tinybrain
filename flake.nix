d797deefcb938b7ea19f2e1749b886abf730fffa
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2025-11-18 23:35:52 +0100

    DBT fix to capture stdin in qemu

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    dbt.url = "github:javier-varez/dbt";
+    dbt.url = "github:javier-varez/dbt/runOutOfNinja";
   };
 
   outputs =
